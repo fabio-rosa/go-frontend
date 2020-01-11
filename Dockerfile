6bb3e08374817a1463f7a015cfd83f56a832d366
fabio-rosa <!P@tch##1>
2020-01-11 17:56:16 +0000

    first commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+FROM golang:alpine
+RUN mkdir /app
+ADD . /app/
+WORKDIR /app
+RUN go build -o main
+CMD ["/app/main"]
+
+# Expose Port
+EXPOSE 8080
+
+
+
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", HelloServer)
+  http.ListenAndServe(":8080", nil)
+  fmt.Println("Serving in container port 8080..")
+}
+
+func HelloServer(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello from frontend")
+}
